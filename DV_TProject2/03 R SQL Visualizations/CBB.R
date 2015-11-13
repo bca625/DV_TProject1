61c340705114414802c2fafc6b682f24cebb4e8b
Bryan Amaefule <bca625@gmail.com>
2015-11-13 11:37:38 -0600

    finally got the first bar chart working

--- DV_TProject2/03 R SQL Visualizations/CBB.R
+++ DV_TProject2/03 R SQL Visualizations/CBB.R
@@ -9,16 +9,9 @@ require(dplyr)
 KPI_Low_Max_value = 3     
 KPI_Medium_Max_value = 7
 
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query= "select color, clarity, sum_price, round(sum_carat) as sum_carat, kpi as ratio, 
-case
-when kpi < "p1" then \\\'03 Low\\\'
-when kpi < "p2" then \\\'02 Medium\\\'
-else \\\'01 High\\\'
-end kpi
-from (select color, clarity, 
-  sum(price) as sum_price, sum(carat) as sum_carat, 
-  sum(price) / sum(carat) as kpi
-  from CBB
-  group by color, clarity)
-order by clarity;"
-                                                ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_ba7433', PASS='orcl_ba7433', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(df)
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query= "select * from CBB order by pos;"
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_ba7433', PASS='orcl_ba7433', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(df)
+
+#Produce fisrt bar chart, rotated on its side so you can actually read the conferences
+ggplot(df, aes(CONF, PTS, fill=POS)) + geom_bar(stat="identity") + coord_flip()
+
