6fb5217e3f63888e472711c2041d822dbcd1d0dd
Bryan Amaefule <bca625@gmail.com>
2015-11-13 12:41:03 -0600

    both bar charts working

--- DV_TProject2/03 R SQL Visualizations/CBB.R
+++ DV_TProject2/03 R SQL Visualizations/CBB.R
@@ -12,6 +12,12 @@ KPI_Medium_Max_value = 7
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query= "select * from CBB order by pos;"
 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_ba7433', PASS='orcl_ba7433', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(df)
 
+
 #Produce fisrt bar chart, rotated on its side so you can actually read the conferences
+ggplot(df, aes(CONF, PTS, fill=CONF)) + geom_bar(stat="identity") + coord_flip()
+
+
+#Produce Second bar chart, rotated on its side so you can actually read the conferences
 ggplot(df, aes(CONF, PTS, fill=POS)) + geom_bar(stat="identity") + coord_flip()
+
 
